bde6e8964cb57a38039d6e2c5f94c744032ba97e
priyasree1701 <priyasree.manthri@gmail.com>
2020-07-21 23:58:53 +0530

    task 10

---
+++ task10/src/task10/CombinationOption.java
@@ -0,0 +1,10 @@
+package task10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+public enum CombinationOption {
+	 INCLUDE_DUPLICATED,
+	    EXCLUDE_DUPLICATED
+}
